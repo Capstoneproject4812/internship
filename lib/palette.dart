53a1fcadc326015d1abcded8354160adbf428f13
Capstoneproject4812 <capstoneproject4812@gmail.com>
2021-01-03 21:53:18 +0530

    Login UI

---
+++ lib/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+const TextStyle Heading = TextStyle(
+    fontSize: 60.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+);
+
+const TextStyle BodyText = TextStyle(
+    fontSize: 19,
+    color: Colors.white,
+);
